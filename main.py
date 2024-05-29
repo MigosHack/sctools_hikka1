b9fcb6f7404b95a51afb67c77bdbbf5a74344655
ZoroteX <111179648+MigosHack@users.noreply.github.com>
2024-05-29 20:57:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
+import colorama
+
 import os
-folders = ['for-update-csv', 'actual-csv', 'updated-csv']
-for folder in folders:
-    if not os.path.exists(folder):
-        os.mkdir(folder)
-        print(f"[Csv Update Master] The {folder} folder for working with files was not found!\nFolder {folder} has been created successfully\n")
-if not os.listdir('actual-csv'):
-    print("[Csv Update Master] There are no files in the actual-csv folder. Please put the files in it\n")
-elif not os.listdir('for-update-csv'):
-    print("[Csv Update Master] There are no files in the for-update-csv folder. Please put the files in it\n")
-else:
-    from system.utils import updater
+
+import telethon
+
+import asyncio
+
+
+
+
+
+from .. import loader, utils
+
+
+
+# developer -> @zorotexr
+
+# developer SCTex -> @daniilsivi
+
+# developer mp42sc -> @daniilsivi
+
+
+
+@loader.tds
+
+class SCToolsMod(loader.Module):
+
+    """SCTools for Brawl Stars"""
+
+
+
+    strings = {"name": "SCTools"}
+
+
+
+    @loader.owner
+
+    async def sc2pngcmd(self, message):
+
+        await message.edit("dudka technology")
