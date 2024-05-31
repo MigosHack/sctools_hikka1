7e962ea9c2c1107e85a1f6e5d2e8b607c5e0945a
ZoroteX <111179648+MigosHack@users.noreply.github.com>
2024-05-31 20:45:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,37 @@
-import colorama
-
-import os
-
-import telethon
-
-import asyncio
-
-
-
-
-
-from .. import loader, utils
-
-
-
-# developer -> @zorotexr
-
-# developer SCTex -> @daniilsivi
-
-# developer mp42sc -> @daniilsivi
-
-
-
-@loader.tds
-
-class SCToolsMod(loader.Module):
-
-    """SCTools for Brawl Stars"""
-
-
-
-    strings = {"name": "SCTools"}
-
-
-
-    @loader.owner
-
-    async def sc2pngcmd(self, message):
-
-        await message.edit("dudka technology")
-        print("syka dinaxy")
+import colorama 
+import os 
+import telethon 
+import asyncio 
+ 
+ 
+from .. import loader, utils 
+ 
+# developer -> @zorotexr 
+# developer SCTex -> @daniilsivi 
+# developer mp42sc -> @daniilsivi 
+ 
+@loader.tds 
+class SCToolsMod(loader.Module): 
+    """SCTools for Brawl Stars""" 
+ 
+    strings = {"name": "SCTools"} 
+ 
+    @loader.owner 
+    async def sc2pngcmd(self, message): 
+        """Decode SC""" 
+        print("динаху декодим ск") 
+        await message.edit("dudka technolog(decodesc)") 
+ 
+        # эм чо ну типо асунк селф таскс чето там вообще 
+ 
+    async def png2sccmd(self, message): 
+        """Encode SC""" 
+        print("динаху енкодим ск") 
+        await message.edt("dudkaaaaaa(encode sc)") 
+ 
+    async def mp42sccmd(self, message): 
+        """Сделать SC из MP4, GIF""" 
+        while True: 
+            print("ааа коляски") 
+ 
+            continue
